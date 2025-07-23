72b485533b35417437ddff318df446edc0f59c19
AlexMuhammad <saputraalex872@gmail.com>
2025-07-23 15:50:23 +0700

    feat: update README and enhance vault interface with mint functionality

--- components/vault-interface.tsx
+++ components/vault-interface.tsx
@@ -11,9 +11,9 @@ export function VaultInterface() {
   return (
     <Card className="w-full max-w-md py-0 gap-0 px-0">
       <CardHeader className="bg-orange-700 rounded-t-xl py-6">
-        <CardTitle className="text-white">Your Balance</CardTitle>
+        <CardTitle className="text-white">Vault Balance</CardTitle>
         <CardDescription className="text-2xl font-bold text-white">
-          9.90
+          9.90 <span className="text-sm font-normal">vMUSDC</span>
         </CardDescription>
       </CardHeader>
       <CardContent className="px-0 border-none">
--- components/vault-tabs.tsx
+++ components/vault-tabs.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+type AmountType = {
+  deposit: string;
+  withdraw: string;
+  mint: string;
+};
+
+type enumAmountType = "deposit" | "withdraw" | "mint";
+
 export function VaultTabs() {
+  const [amount, setAmount] = useState<AmountType>({
+    deposit: "",
+    withdraw: "",
+    mint: "",
+  });
+
+  const handleAmountChange =
+    (type: enumAmountType) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      let { value } = e.target;
+      value = value.replace(/[^0-9]/g, "");
+      setAmount((prev) => ({
+        ...prev,
+        [type]: value,
+      }));
+    };
+
   return (
     <Tabs defaultValue="deposit">
       <TabsList className="w-full rounded-none h-12 p-0 shadow-none border-none">
@@ -26,16 +48,27 @@ export function VaultTabs() {
         >
           Withdraw
         </TabsTrigger>
+        <TabsTrigger
+          value="mint"
+          className="rounded-none data-[state=active]:shadow-none data-[state=active]:border-b-orange-600"
+        >
+          Mint
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="deposit">
         <Card className="rounded-t-none gap-0 border-none shadow-none">
           <CardContent>
             <div className="space-y-3">
-              <Label htmlFor="amount-deposit">Deposit Amount</Label>
+              <div className="flex items-center justify-between">
+                <Label htmlFor="amount-deposit">Deposit Amount</Label>
+                <span className="text-xs">0.0 MUSDC</span>
+              </div>
               <Input
                 id="amount-deposit"
                 type="number"
                 placeholder="Enter your amount..."
+                value={amount.deposit}
+                onChange={handleAmountChange("deposit")}
               />
             </div>
           </CardContent>
@@ -53,11 +86,16 @@ export function VaultTabs() {
         <Card className="rounded-t-none gap-0 border-none shadow-none">
           <CardContent>
             <div className="space-y-3">
-              <Label htmlFor="amount-withdraw">Withdraw Amount</Label>
+              <div className="flex items-center justify-between">
+                <Label htmlFor="amount-withdraw">Withdraw Amount</Label>
+                <span className="text-xs">0.0 MUSDC</span>
+              </div>
               <Input
                 id="amount-withdraw"
                 type="number"
                 placeholder="Enter your amount..."
+                value={amount.withdraw}
+                onChange={handleAmountChange("withdraw")}
               />
             </div>
           </CardContent>
@@ -67,6 +105,29 @@ export function VaultTabs() {
               className="w-full bg-orange-700 hover:bg-orange-600"
             >
               Withdraw
+            </Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="mint">
+        <Card className="rounded-t-none gap-0 border-none shadow-none">
+          <CardContent>
+            <div className="space-y-3">
+              <Label htmlFor="amount-mint">Mint Amount</Label>
+              <Input
+                id="amount-mint"
+                placeholder="Enter your amount..."
+                value={amount.mint}
+                onChange={handleAmountChange("mint")}
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="mt-3">
+            <Button
+              size={"lg"}
+              className="w-full bg-orange-700 hover:bg-orange-600"
+            >
+              Mint
             </Button>
           </CardFooter>
         </Card>
